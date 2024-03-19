33571387b18b78f48de3f36e9aa093a6edec1a37
Rositsa <rositsa@yahoo.com>
2024-03-19 15:44:43 +0200

    shuffle

--- towns.js
+++ towns.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$('#btnAdd').click(addTown);
+	$('#btnShuffle').click(shuffleTowns);
 	$('#btnDelete').click(deleteTown)
 });
 
@@ -9,6 +10,24 @@ function addTown() {
 	$('#towns').append($('<option>').text(townName));
 	$('#result').text(townName + " added.");
 }
+
+function shuffleTowns() {
+	let towns = $('#towns option').toArray();
+	$('#towns').empty();
+	shuffleArray(towns);
+	$('#towns').append(towns);
+	$('#result').text("Towns shuffled.");
+
+	function shuffleArray(array) {
+		for (var i = array.length - 1; i > 0; i--) {
+			var j = Math.floor(Math.random() * (i + 1));
+			var oldElement = array[i];
+			array[i] = array[j];
+			array[j] = oldElement;
+		}
+	}
+}
+
 
 
 function deleteTown() {
